6e71a347e4109803af85018d3e6982fa8661c6d5
Igor Gonçalves <igordossantos@gmail.com>
2021-09-25 19:24:20 -0300

    Não pensei nos problemas antes codar

---
+++ ex04.js
@@ -0,0 +1,23 @@
+/*
+function whatsThisMonth(number) {
+    const months = {
+        jan: 1,
+        fev: 2,
+        mar: 2,
+        abr: 4,
+        mai: 5,
+        jun: 6,
+        jul: 7,
+        ago: 8,
+        set: 9,
+        out: 10,
+        nov: 11,
+        dez: 12
+    }
+    if(Math.random(number, 12) == number) {
+
+    }
+}
+*/
+
+// Utilizar uma estrutura Switch seria mais inteligente. 
