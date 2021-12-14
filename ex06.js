dbc4dae03a138a1502b67185a513f6d887f2e7b4
Igor Gonçalves <igordossantos38@gmail.com>
2021-12-14 19:57:18 -0300

    Tentando achar a lógica de inversão

---
+++ ex06.js
@@ -0,0 +1,16 @@
+// Função vai ser um bolean ou number
+
+// Se bolean return inverso da entrada
+
+// Se number return inverso --> 1 return -1
+
+// Se !number ou !bolean return "booleano ou número esperados, mas o parâmetro é do tipo ..."
+
+function inversora(valor = 6) {
+    // console.log(valor === Number)
+    if (valor === Number) {
+        console.log(valor = valor + (- valor))
+    }
+    return valor
+}
+inversora()
