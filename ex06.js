be2ccfb9918bd9bbd03b6bcd9dddcb000fb3895b
Igor Gonçalves <igordossantos38@gmail.com>
2021-12-15 13:38:44 -0300

    Só falta arrumar a parte de verificação do valor falso

--- ex06.js
+++ ex06.js
@@ -1,19 +1,25 @@
-// Função vai ser um bolean ou number
+//// Função vai ser um bolean ou number
 
 // Se bolean return inverso da entrada
 
-// Se number return inverso --> 1 return -1
+//// Se number return inverso --> 1 return -1
 
 // Se !number ou !bolean return "booleano ou número esperados, mas o parâmetro é do tipo ..."
 
 function inversora(valor) {
-    // console.log(valor === Number)
     if (valor > 0 || valor < 0) {
-        console.log(valor * (- 1))
-    } else if (valor === true || valor === false) {
-        console.log(!!valor)
+        valor  *= (- 1)
+    } else if (valor) {
+        valor = typeof valor
+        return valor
+    } else if (!valor) {
+        valor = true
+    } else {
+        valor = 'Valor esperado bolean ou number'
     }
     return valor
 }
-inversora(6)
-
+console.log(inversora(-12))
+console.log(inversora(false))
+console.log(inversora(true))
+console.log(inversora('igor'))
