f09c1de5f94a8e234b4da83ae31d77f9a23f37f3
Igor Gonçalves <igordossantos38@gmail.com>
2021-12-23 11:15:40 -0300

    Exercício mal elaborado

---
+++ ex07.js
@@ -0,0 +1,11 @@
+//// Função com 4 parametros (numero, minimo, maximo, inclusivo)
+
+// Retorne se o parametro numero esta entre o minimo e o maximo
+
+// O parametro inclusivo é um bolean
+
+function entre(numero, min, max, inclusivo) {
+    
+}
+
+entre(13, 50, 200)
