ae82be5ee2ab1ae93c3aae5604f128d291090543
Igor Gonçalves <igordossantos38@gmail.com>
2021-12-26 00:46:53 -0300

    Exercicio concluido com sucesso usando documentação da linguagem

--- ex13.js
+++ ex13.js
@@ -4,11 +4,11 @@
 
 function onlyNumbers(array) {
     const newArray = array.filter((e) => {
-        if (e >= 0) {
+        if (typeof(e) === 'number' && !isNaN(e)) {
             return e
         }
     })
     console.log(newArray)
 }
-onlyNumbers(["a", "Igor", true, 1, "c", 4])
-// onlyNumbers(["a", "Igor", true, 4, "c", 8, "9", 10])
+onlyNumbers(["a", "Igor", true, 1, "c", 4, 0, -1])
+onlyNumbers(["a", "Igor", true, 4, "c", 8, "9", 10])
