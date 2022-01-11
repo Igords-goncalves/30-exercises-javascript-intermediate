9cf5904917f6faa4882184ab2d7e3207d354ac05
Igor Gonçalves <igordossantos38@gmail.com>
2022-01-11 17:57:34 -0300

    Exercício executado com sucesso

--- ex29.js
+++ ex29.js
@@ -1,14 +1,21 @@
 // Função retorna o segundo numero maior de um array
 
 function segundoMaiorDoArray(arr) {
-    let maior = 0 
-    let menor = 0
-    let segundoMaior = 0
+    let maior = 0
+    let segundoMaior
 
     for (let i = 0; i < arr.length; i++) {
         arr[i] > maior ? maior = arr[i] : 0
-        arr[i] < i ? menor = arr[i] : 0
+
+        arr.splice(i, 1)
+        segundoMaior = arr[0]
+    
+        arr.forEach(element => {
+            if(element > segundoMaior)
+            segundoMaior = element
+        })
     }
     console.log(maior)
+    console.log(segundoMaior)
 }
-segundoMaiorDoArray([12, 28, 9, 44])
+segundoMaiorDoArray([12, 28, 42, 10, 9, 44, 2])
